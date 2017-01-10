ea8be72d4bd5aef7938b5016c2e63f5406d6b2af
BigBlueHat <byoung@bigbluehat.com>
2017-01-10 16:32:43 -0500

    Add Vue.js to the mix; refactor for awesome

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var Vue = require('vue');
+
 var levelgraph = require('levelgraph');
 var leveljs = require('level-js');
 var levelup = require('levelup');
@@ -12,63 +14,63 @@ var db = levelgraphJSONLD(
 );
 window.db = db;
 
+var default_doc = {
+  "@context": {
+    "name": "http://xmlns.com/foaf/0.1/name",
+    "homepage": {
+      "@id": "http://xmlns.com/foaf/0.1/homepage",
+      "@type": "@id"
+    }
+  },
+  "@id": "http://manu.sporny.org#person",
+  "name": "Manu Sporny",
+  "homepage": "http://manu.sporny.org/"
+};
 
-function loadSPOTable(list) {
-  function addTd(tr, value) {
-    var td = document.createElement('td');
-    td.textContent = value;
-    tr.appendChild(td);
+var app = new Vue({
+  el: '#app',
+  data: {
+    input: default_doc,
+    table: [],
+    filter_by: {
+      subject: '',
+      predicate: '',
+      object: ''
+    }
+  },
+  created: function() {
+    this.displayTriples({});
+  },
+  methods: {
+    displayTriples: function(spo) {
+      var self = this;
+      db.get(spo, function(err, list) {
+        self.table = list;
+      });
+    },
+    put: function() {
+      var self = this;
+      db.jsonld.put(this.input, function(err, obj) {
+        // do something after the obj is inserted
+        console.log(err, obj);
+        self.displayTriples({});
+      });
+    },
+    filter: function() {
+      var spo = {
+        subject: this.filter_by.subject.trim(),
+        predicate: this.filter_by.predicate.trim(),
+        // objects are stored in levelgraph (at least via the JSON-LD addon) as strings in quotes
+        // TODO: turn into computed property
+        object: '"' + this.filter_by.object.trim()
+                  .replace(/^\"/, '').replace(/\"$/, '') + '"'
+      };
+
+      if (spo.subject === '') delete spo.subject;
+      if (spo.predicate === '') delete spo.predicate;
+      if (spo.object === '""') delete spo.object;
+
+      this.displayTriples(spo);
+    }
   }
-
-  var $tbody = document.querySelector('#spo-table tbody');
-
-  // empty tbody...
-  while ($tbody.hasChildNodes()) {
-    $tbody.removeChild($tbody.lastChild);
-  }
-
-  list.forEach(function(obj) {
-    var tr = document.createElement('tr');
-    addTd(tr, obj.subject);
-    addTd(tr, obj.predicate);
-    addTd(tr, obj.object);
-
-    $tbody.appendChild(tr);
-  });
-}
-
-function displayTriples(spo) {
-  db.get(spo, function(err, list) {
-    loadSPOTable(list);
-  });
-}
-
-var $input = document.getElementById('input');
-
-document.getElementById('put-button').addEventListener('click', function() {
-  db.jsonld.put(JSON.parse($input.value), function(err, obj) {
-    // do something after the obj is inserted
-    console.log(err, obj);
-    displayTriples({});
-  });
 });
-
-displayTriples({});
-
-document.getElementById('search')
-  .addEventListener('click', function() {
-    var spo = {
-      subject: document.getElementById('search-subject').value.trim(),
-      predicate: document.getElementById('search-predicate').value.trim(),
-      // objects are stored in levelgraph (at least via the JSON-LD addon) as strings in quotes
-      object: '"' + document.getElementById('search-object').value.trim()
-                .replace(/^\"/, '').replace(/\"$/, '') + '"'
-    };
-
-    if (spo.subject === '') delete spo.subject;
-    if (spo.predicate === '') delete spo.predicate;
-    if (spo.object === '""') delete spo.object;
-
-    console.log('spo', spo);
-    displayTriples(spo);
-  });
